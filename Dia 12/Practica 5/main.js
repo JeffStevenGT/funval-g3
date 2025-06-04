aca5f3810a77487c620b0aa345147da23047b162
JeffGT <jeff.giltoribio@gmail.com>
2025-06-04 08:01:17 -0500

    practica calificada 5

--- Dia 12/Practica 5/main.js
+++ Dia 12/Practica 5/main.js
@@ -6,15 +6,15 @@ let operacion = prompt(`Seleccione la operacion que desea realizar:
 
 if (operacion === "1") {
     let lado = parseFloat(prompt("ingrese el lado del cuadrado"));
-    console.log("el area del cuadrado es: " + (lado * lado));
+    alert("el area del cuadrado es: " + (lado * lado));
 }   else if (operacion === "2") {
     let base1 = parseFloat(prompt("ingrese la base del rectangulo "));
     let altura1 = parseFloat(prompt("ingrese la altura del rectangulo "));
-    console.log("el area del rectangulo es: " + (base1 * altura1));
+    alert("el area del rectangulo es: " + (base1 * altura1));
 }   else if (operacion === "3") {
     let base = parseFloat(prompt("ingrese la base del triangulo "));
     let altura = parseFloat(prompt("ingrese la altura del triangulo "));
     console.log("el area del triangulop es: " + (base * altura)/2);    
 } else {
-    console.log("la opcion no es valida ._.! intenta nuevamente!")    
+    alert("la opcion no es valida ._.! intenta nuevamente!")    
 }
